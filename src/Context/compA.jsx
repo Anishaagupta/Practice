8db41ebaa5fc647ec1d771668f7e574552e34ef0
Anishaagupta <anisha8057@gmail.com>
2020-07-28 17:45:33 +0530

    --- Auto-Git Commit ---

--- src/Context/compA.jsx
+++ src/Context/compA.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { FirstName, LastName, Age, Course } from '../App';
 
 const CompA = () => {
@@ -6,6 +6,11 @@ const CompA = () => {
   const lname = useContext(LastName);
   const age = useContext(Age);
   const course = useContext(Course);
+  const [num, setNum] = useState(0);
+  useEffect(() => {
+    //alert(`you clicked ${num} times`);
+    document.title`You clicked ${num} times`;
+  });
   return (
     <div>
       <h1>
@@ -15,6 +20,14 @@ const CompA = () => {
       <h1>
         And my Age is {age} and course is {course}
       </h1>
+      <h2>{num}</h2>
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 };
